0bd647753e03829e89a6d712a6331ef2f2e8ca7f
Sócrates Díaz <sdsxd1800@gmail.com>
2021-04-12 12:12:44 -0400

    Work with templates in test suite

--- test/build_file_structure_command_test.dart
+++ test/build_file_structure_command_test.dart
@@ -1,25 +1,33 @@
 import 'dart:io';
 
+import 'package:mustache_template/mustache_template.dart';
 import 'package:test/test.dart';
 import 'package:yaml/yaml.dart';
 
 void main() {
-  test('Create basic structure', () {
-    // Check if pubspec.yaml is available
-    final insideProjectDir = File('pubspec.yaml').existsSync();
-    if (insideProjectDir) {
-      final pubspec =
-          loadYaml('name: test_app\ndescription: test app for test suite');
-      // Get project name from pubspect.yaml
-      final app_name = pubspec['name'];
-      // Use project name for package imports
-      // Build folder structure
-      buildFolderStructure(app_name);
-    }
+  group('Project structure', () {
+    final test_path = 'test/test_bin';
+    test('Create basic structure', () {
+      // Check if pubspec.yaml is available
+      final insideProjectDir = File('pubspec.yaml').existsSync();
+      if (insideProjectDir) {
+        final pubspec =
+            loadYaml('name: test_app\ndescription: test app for test suite');
+        // Get project name from pubspect.yaml
+        final app_name = pubspec['name'];
+        // Use project name for package imports
+        // Build folder structure
+        buildFolderStructure(app_name, test_path);
+      }
+    });
+
+    tearDown(() {
+      // Directory(test_path).deleteSync(recursive: true);
+    });
   });
 }
 
-void buildFolderStructure(app_name) {
+void buildFolderStructure(app_name, path) {
   // File structure:
   // - app/
   //  - blocs/
@@ -61,6 +69,26 @@ void buildFolderStructure(app_name) {
     'l10n'
   ];
   for (var d in directories) {
-    Directory('test/bin/$d').createSync(recursive: true);
+    Directory('${Directory(path).path}/$d').createSync(recursive: true);
   }
+
+  final filesAndAssets = {
+    'app/blocs/simple_bloc_observer.dart':
+        'assets/templates/simple_bloc_observer.dart.template',
+    'app/app.dart': '',
+    'app/navigator.dart': '',
+    'data/constants.dart': '',
+    'di/di.dart': ''
+  };
+  filesAndAssets.forEach((filePath, templatePath) {
+    if (templatePath.isNotEmpty) {
+      final file = File('${Directory(path).path}/$filePath')
+        ..createSync(recursive: true);
+      final templateFileSource = File(templatePath).readAsStringSync();
+      final template =
+          Template(templateFileSource, name: file.uri.pathSegments.last);
+      final renderedTemplate = template.renderString({});
+      file.writeAsStringSync(renderedTemplate);
+    }
+  });
 }
